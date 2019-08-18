8e7753b21749bf78acfd8865bd5091fa8529b85b
mogace <254033181@qq.com>
2019-08-18 09:08:36 +0800

    正则表达式

---
+++ 01_backend/05.php
@@ -0,0 +1,20 @@
+<?php 
+/**
+ * 正则表达式
+ * @date 2019/08/18
+ * @author mogace
+ * 
+ */
+
+$str = 'this is reg
+Reg
+this is
+regexp turtor,oh reg';
+if (preg_match_all('%.*reg$%mi', $str, $arr)) {
+	var_dump($arr);
+} else {
+	var_dump($arr);
+}
+
+
+ ?>
