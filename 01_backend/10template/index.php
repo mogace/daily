0988aeb5a558dff4b5b238a102356ed414008a87
mogace <254033181@qq.com>
2019-11-10 19:31:39 +0800

    Update index.php

--- 01_backend/10template/index.php
+++ 01_backend/10template/index.php
@@ -7,7 +7,7 @@ $template = new Template(['php_turn' => true, 'debug' => true]);
 
 $template->assign('data', 'hello world');
 $template->assign('person', 'cafeCAT');
-$template->assign('pai', 3.14);
+$template->assign('pai',  3.14);
 
 $arr = [1, 2, 3, 4, 'haha', 6];
 $template->assign('b', $arr);
